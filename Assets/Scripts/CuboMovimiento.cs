6bfbb3e2f722e37b5d801bcb4abc1c353e7fded4
Martincho250 <50048880@est.ort.edu.ar>
2026-05-06 14:33:31 -0300

    Initial commit

---
+++ Assets/Scripts/CuboMovimiento.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CuboMovimiento : MonoBehaviour
+{
+    public float speed;
+    // Start is called before the first frame update
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(0,-speed, 0);
+    }
+}
